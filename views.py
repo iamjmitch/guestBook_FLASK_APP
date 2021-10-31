9cb8aba1f3dd5e8416b56fc95660366cf4a38eca
James Mitchell <iamjmitch1990@gmail.com>
2021-10-31 23:24:36 +1000

    updated to work on heroku

--- views.py
+++ views.py
@@ -1,27 +1,29 @@
 from flask import Blueprint, render_template, request, redirect, url_for
 
-from . import db
-from .models import Comment
-
 main = Blueprint('main', __name__)
+
+class GuestBookEntry:
+    def __init__(self, name, comment):
+        self.name = name
+        self.comment = comment
+
+guestbook = []
 
 @main.route('/')
 def index():
-    comments = Comment.query.all()
-    return render_template('index.html', comments=comments)
+    return render_template('index.html', guestbook=guestbook)
 
 
 @main.route('/sign')
 def sign():
     return render_template('sign.html')
 
-
 @main.route('/sign', methods=['POST'])
 def sign_post():
     name = request.form.get('name')
     comment = request.form.get('comment')
-    new_comment = Comment(name=name, comment_text=comment)
-    db.session.add(new_comment)
-    db.session.commit()
+    new_comment = GuestBookEntry(name, comment)
+    guestbook.append(new_comment)
+    
     
     return redirect(url_for('main.index'))
