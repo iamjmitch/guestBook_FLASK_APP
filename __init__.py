49b2401734a9460a7f55434ae4da668ae220e129
James Mitchell <iamjmitch1990@gmail.com>
2021-10-31 23:43:20 +1000

    attempted heroku fix

--- __init__.py
+++ __init__.py
@@ -1,11 +1,9 @@
 # Imports
 from flask import Flask
 
-def create_app():
-    app = Flask(__name__)
+app = Flask(__name__)
+from .views import main
+app.register_blueprint(main)
+app.run
 
-    from .views import main
-    app.register_blueprint(main)
-
-    return app
 
